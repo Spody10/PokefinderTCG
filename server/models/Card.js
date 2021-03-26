ebac4dc85f2be1c323a8bacf33ed2582fa8c450f
Jessica Mangiameli <jmangiam@asu.edu>
2021-03-26 10:29:19 -0500

    models and connection

---
+++ server/models/Card.js
@@ -0,0 +1,42 @@
+const { Schema } = require('mongoose');
+
+const cardSchema = new Schema({
+    cardId: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    link: {
+      type: String,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    grading: {
+        type: String,
+    },
+    holo: {
+        type: Boolean,
+    },
+    superRare: {
+        type: Boolean,
+    },
+    secretRare: {
+        type: Boolean,
+    },
+    promo: {
+        type: Boolean,
+    },
+    reverse: {
+        type: Boolean,
+    },
+    sold: {
+        type: Boolean,
+    }    
+  });
+  
+  module.exports = bookSchema;
+  
