00131dca2f4820154a804c1bf6c300834a35f961
Michael <michaelpspore@gmail.com>
2021-03-26 11:33:48 -0500

    connection test complete

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+// import React from 'react'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// function App() {
+//     return (
+//       <Router>
+//         <>
+//           <Navbar />
+//           <Switch>
+//             <Route  />
+//             <Route  />
+//             <Route render={() => <h1>Wrong page!</h1>} />
+//           </Switch>
+//         </>
+//       </Router>
+//     );
+//   }
+  
+//   export default App;
