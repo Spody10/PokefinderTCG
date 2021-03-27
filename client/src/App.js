3a3e9da73bb78e5b0067d8440645ad54c62f899b
BeatyPete <akira941@gmail.com>
2021-03-27 01:00:31 -0500

    added necessary packages and missing file contents to get test login.js to pull up

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+import Login from './pages/Login'
+
+/* const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql',
+}) */
+
+function App() {
+  return (
+    <Login />
+  );
+}
+
+export default App;
