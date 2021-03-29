b2e59833653170bc308783a1c8a9fe945bbbf58b
CoCo Chanel <veta583518@gmail.com>
2021-03-28 20:35:20 -0500

    created some HTML for Collection Page, Card Component & Pokedeck component

--- client/src/pages/Collection.js
+++ client/src/pages/Collection.js
@@ -1,11 +1,21 @@
 import React from "react";
 import pokebox from "../assets/images/pokebox.png";
 import Header from "../components/Header/index";
+import Card from "../components/Card";
 
 function Collection() {
   return;
   <div>
-    <img src={pokebox} style={{ width: "100%" }} alt="pokebox" />
+    <Header />
+    <div className="title-plaque">
+      <h1>Collection</h1>
+    </div>
+    <div className="pokebox-container">
+      <img src={pokebox} style={{ width: "100%" }} alt="pokebox" />
+      <div className="pokebox-row">
+        <Card />
+      </div>
+    </div>
   </div>;
 }
 
