1fc638f74a9df6ba4abd9e43f9028369e766989e
BeatyPete <akira941@gmail.com>
2021-03-30 02:01:27 -0500

    styled more pokebox and box label

--- client/src/components/Pokebox/index.js
+++ client/src/components/Pokebox/index.js
@@ -4,9 +4,16 @@ import "./pokebox.css";
 
 const Header = () => {
     return (
-        <section className="box-container">
-        <div className='pokebox'>
+      <section className="box-container">
 
+        <div className='box-label-container'>
+          <div id='left-triangle'></div>
+          <div className='box-label'></div>
+          <div id='right-triangle'></div>
+        </div>
+
+        <div className='pokebox'>
+          
         </div>
       </section>
     );
