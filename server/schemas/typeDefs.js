6e455d1f6a605ebe773b0326bab01e3d35727394
BeatyPete <akira941@gmail.com>
2021-03-30 04:53:38 -0500

    added setname to card model and schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,7 @@ const typeDefs = gql`
   type Card {
     _id: ID
     name: String
+    setName: String
     email: String
     image: String
     grading: String
@@ -37,7 +38,7 @@ const typeDefs = gql`
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): User
-    addCard(name: String!, email: String!, image: String!, grading: String, holo: Boolean, reverse: Boolean, superRare: Boolean, secretRare: Boolean, promo: Boolean, askingPrice: Float!, sold: Boolean): Card
+    addCard(name: String!, setName: String, email: String!, image: String!, grading: String, holo: Boolean, reverse: Boolean, superRare: Boolean, secretRare: Boolean, promo: Boolean, askingPrice: Float!, sold: Boolean): Card
     login(email: String!, password: String!): Auth
   }
 `;
