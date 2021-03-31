065dee0219ca19a62e47697fd3b4ec5b04832a06
CoCo Chanel <veta583518@gmail.com>
2021-03-30 19:15:10 -0500

    start to collection page

--- client/src/pages/Collection.js
+++ client/src/pages/Collection.js
@@ -1,22 +1,22 @@
 import React from "react";
 import pokebox from "../assets/images/pokebox.png";
-import Header from "../components/Header/index";
-import Card from "../components/Card";
 
-function Collection() {
-  return;
-  <div>
-    <Header />
-    <div className="title-plaque">
-      <h1>Collection</h1>
-    </div>
-    <div className="pokebox-container">
-      <img src={pokebox} style={{ width: "100%" }} alt="pokebox" />
-      <div className="pokebox-row">
-        <Card />
+const Collection = () => {
+  return (
+    <main>
+      <div>
+        /* title plaque */
+        <div className="text-box title-plaque">
+          <h1 className="center-text page-title">Collection</h1>
+        </div>
+        /* pokebox */
+        <div className="pokebox-container">
+          /* cards start */
+          <div className="pokebox-row"></div>
+        </div>
       </div>
-    </div>
-  </div>;
-}
+    </main>
+  );
+};
 
 export default Collection;
